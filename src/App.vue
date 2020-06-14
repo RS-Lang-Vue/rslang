04849c6261b28aa7368d5ee002a1ffd3da76b89b
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-14 19:42:34 +0600

    feat: create SideNav component and include it into Main layout

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,10 @@
     <router-view />
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/views/layouts/Main.vue
+++ src/views/layouts/Main.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="main-layout">
-    <router-view></router-view>
+    <side-nav class="main-layout__side-nav"></side-nav>
+    <router-view class="main-layout__content"></router-view>
   </div>
 </template>
+
+<script>
+import SideNav from "@/components/common/sideNav/SideNav.vue";
+
+export default {
+  components: {
+    "side-nav": SideNav,
+  },
+};
+</script>
+
+<style scoped>
+.main-layout {
+  display: flex;
+  justify-content: space-between;
+}
+
+.main-layout__side-nav {
+  width: 30%;
+  height: 100vh;
+  box-sizing: border-box;
+}
+
+.main-layout__content {
+  width: 70%;
+  box-sizing: border-box;
+}
+</style>
