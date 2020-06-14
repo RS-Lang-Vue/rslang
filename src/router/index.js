4f9d7898bdb0d2effa6dc9541137ed7707b6f44f
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-14 19:05:56 +0600

    feat: integrate prettier into project

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const routes = [];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
