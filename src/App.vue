13bb3aa60125ce8a82d73b6f871ecb892163dd24
Anton <akrayushkin@yandex.ru>
2020-06-17 16:01:06 +0300

    feat: add vuetifyjs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
