61c4f6c4b553b1cdbb2ce5cae1b086df756bbe43
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-21 20:41:13 +0600

    refactor: put markup of nav drawer into single component

--- src/views/layouts/Main.vue
+++ src/views/layouts/Main.vue
@@ -1,150 +1,6 @@
 <template>
   <div class="main-layout">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list>
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/women/82.jpg"></v-img>
-          </v-list-item-avatar>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title class="title">Sandra Adams</v-list-item-title>
-            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item link to="/home">
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link to="/team">
-          <v-list-item-action>
-            <v-icon>mdi-account-group</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>О команде</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link to="/stats">
-          <v-list-item-action>
-            <v-icon>mdi-chart-bar</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Статистика</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link to="/vocabulary">
-          <v-list-item-action>
-            <v-icon>mdi-bookshelf</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Словарь</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link to="/settings">
-          <v-list-item-action>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Настройки</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item link to="/learn">
-          <v-list-item-action>
-            <v-icon>mdi-alphabetical-variant</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Словарная тренировка</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-nintendo-game-boy</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Игра "SpeakIt"</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-nintendo-game-boy</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Игра "English puzzle"</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-nintendo-game-boy</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Игра "Саванна"</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-nintendo-game-boy</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Игра "Аудиовызов"</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-nintendo-game-boy</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Игра "Спринт"</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-nintendo-game-boy</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Игра "Своя игра"</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item @click="handleLogout">
-          <v-list-item-action>
-            <v-icon>mdi-logout</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Выйти</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <side-nav />
 
     <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -167,18 +23,11 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import SideNav from "@/components/common/sideNav/SideNav.vue";
 
 export default {
-  data: () => ({
-    drawer: null,
-  }),
-  methods: {
-    handleLogout() {
-      this.unsetUser();
-      this.$router.push("/auth/login");
-    },
-    ...mapMutations(["unsetUser"]),
+  components: {
+    "side-nav": SideNav,
   },
 };
 </script>
