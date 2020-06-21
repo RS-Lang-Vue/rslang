2aeb0adbce8b6cbf064601c678baca6c5289f18f
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-21 20:13:30 +0600

    feat: add navigation drawer markup into main layout

--- src/views/layouts/Main.vue
+++ src/views/layouts/Main.vue
@@ -1,34 +1,150 @@
 <template>
   <div class="main-layout">
-    <side-nav class="main-layout__side-nav"></side-nav>
-    <router-view class="main-layout__content"></router-view>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/women/82.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-information-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>О приложении</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Настройки</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-alphabetical-variant</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Словарная тренировка</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-nintendo-game-boy</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Игра "SpeakIt"</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-nintendo-game-boy</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Игра "English puzzle"</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-nintendo-game-boy</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Игра "Саванна"</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-nintendo-game-boy</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Игра "Аудиовызов"</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-nintendo-game-boy</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Игра "Спринт"</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-nintendo-game-boy</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Игра "Своя игра"</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>RS Lang - изучение</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center">
+            <router-view class="main-layout__content"></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2020 RS Lang</span>
+    </v-footer>
   </div>
 </template>
 
 <script>
-import SideNav from "@/components/common/sideNav/SideNav.vue";
-
 export default {
-  components: {
-    "side-nav": SideNav,
-  },
+  data: () => ({
+    drawer: null,
+  }),
 };
 </script>
 
 <style scoped>
-.main-layout {
-  display: flex;
-  justify-content: space-between;
-}
-
-.main-layout__side-nav {
-  width: 30%;
-  height: 100vh;
-  box-sizing: border-box;
-}
-
 .main-layout__content {
-  width: 70%;
-  box-sizing: border-box;
+  background-color: red;
 }
 </style>
