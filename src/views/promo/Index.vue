ad002468976d25a861e5b1edc3a51c3f3242ec6f
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-21 20:07:37 +0600

    fix: make text center on the promo page

--- src/views/promo/Index.vue
+++ src/views/promo/Index.vue
@@ -2,7 +2,7 @@
   <v-main class="promo">
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
-        <v-col class="promo__text_center">
+        <v-col class="text-center">
           <h1 class="promo__header">RS Lang</h1>
           <p class="promo__title">
             представляет собой оптимальный интеллектуальный способ изучения слов английского языка
