933902342f1d9a51c5031d1c287c815b027f5383
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-21 20:04:34 +0600

    feat: add markup of promo page

--- src/views/promo/Index.vue
+++ src/views/promo/Index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>Promo page</div>
+  <v-main class="promo">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col class="promo__text_center">
+          <h1 class="promo__header">RS Lang</h1>
+          <p class="promo__title">
+            представляет собой оптимальный интеллектуальный способ изучения слов английского языка
+          </p>
+          <v-btn class="promo__button" large color="primary" @click="$router.push('home')"
+            >Начать</v-btn
+          >
+          <v-btn class="promo__button" text large color="primary">Войти</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
-<script>
-export default {};
-</script>
+<style scoped>
+.promo__header {
+  font-style: normal;
+  font-weight: 300;
+  font-size: 96px;
+  line-height: 112px;
+}
 
-<style></style>
+.promo__title {
+  max-width: 900px;
+  margin: 0 auto;
+  margin-bottom: 24px;
+  font-size: 28px;
+  line-height: 42px;
+  text-align: center;
+}
+
+.promo__button {
+  margin: 24px;
+}
+</style>
