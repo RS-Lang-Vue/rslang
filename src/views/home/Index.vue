dd4edf11ac49a90c2e00eb1ad9838c438869dc0f
Vitali Matusevich <vimatu@gmail.com>
2020-06-24 17:59:42 +0300

    feat: add all cards homepage

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home grey lighten-1">
+  <div class="home">
     <v-container>
       <!-- Stack the columns on mobile by making one full-width and the other half-width -->
       <v-row>
-        <v-col cols="12" md="4">
-          <v-card class="mx-auto" max-width="400">
-            <v-card-title class="headline">Сегодня</v-card-title>
+        <v-col cols="12" md="6" lg="4">
+          <v-card class="mx-auto" max-width="500">
+            <v-card-title class="headline">Статистика</v-card-title>
             <v-simple-table dense light>
               <tbody>
                 <tr>
@@ -32,32 +32,40 @@
             </v-simple-table>
 
             <v-card-actions>
-              <v-btn text color="indigo accent-4">Продолжить обучение</v-btn>
+              <v-btn text color="indigo accent-4">Подробнее</v-btn>
+              <v-btn text color="indigo accent-4">Словарь</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
 
-        <v-col cols="12" md="4">
-          <v-card class="pa-2" outlined tile>
-            .col-6 .col-md-4
+        <v-col cols="12" md="6" lg="4">
+          <v-card class="mx-auto" max-width="500">
+            <v-card-title class="headline">Цель на сегодня</v-card-title>
+            <v-card-text class="text-left">Завершить 50 карточек</v-card-text>
+            <v-card-text class="text-left"
+              >Сегодня вы выполнили 0 карточек. Для достижения цели завершите 50
+              карточек</v-card-text
+            >
+
+            <v-card-actions>
+              <v-btn text color="indigo accent-4">Достигнуть цели</v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
 
-      <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
       <v-row>
-        <v-col v-for="n in 3" :key="n" cols="6" md="4">
-          <v-card class="pa-2" outlined tile>
-            .col-6 .col-md-4
-          </v-card>
-        </v-col>
-      </v-row>
-
-      <!-- Columns are always 50% wide, on mobile and desktop -->
-      <v-row>
-        <v-col v-for="n in 2" :key="n" cols="6">
-          <v-card class="pa-2" outlined tile>
-            .col-6
+        <v-col cols="12" md="6" lg="4" v-for="card in cards" :key="card.id">
+          <v-card color="pink lighten-5" class="mx-auto" max-width="500">
+            <v-card-title class="headline">{{ card.cardTitle }}</v-card-title>
+            <v-card-subtitle class="text-left">{{ card.cardSubtitle }}</v-card-subtitle>
+            <v-card-text class="text-left">{{ card.cardText }}</v-card-text>
+            <v-card-actions>
+              <v-btn text color="indigo accent-4" :to="card.mainButton.link">{{
+                card.mainButton.title
+              }}</v-btn>
+              <v-btn text color="indigo accent-4">{{ card.additionalButton.title }}</v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
@@ -66,7 +74,62 @@
 </template>
 
 <script>
-export default {};
+const cards = [
+  {
+    cardTitle: "SpeakIt",
+    cardSubtitle: "Мини игра",
+    cardText:
+      "Очень краткое описание игры. А возможно и статистика игрока. Типа, сыграно - столько, уровень - такой, изучено слов — столько.",
+    mainButton: { title: "Играть", link: "/games/speak-it" },
+    additionalButton: { title: "Инфо", link: "" },
+  },
+  {
+    cardTitle: "English puzzle",
+    cardSubtitle: "Мини игра",
+    cardText:
+      "Очень краткое описание игры. А возможно и статистика игрока. Типа, сыграно - столько, уровень - такой, изучено слов — столько.",
+    mainButton: { title: "Играть", link: "/games/english-for-kids" },
+    additionalButton: { title: "Инфо", link: "" },
+  },
+  {
+    cardTitle: "Саванна",
+    cardSubtitle: "Мини игра",
+    cardText:
+      "Очень краткое описание игры. А возможно и статистика игрока. Типа, сыграно - столько, уровень - такой, изучено слов — столько.",
+    mainButton: { title: "Играть", link: "/games/savana" },
+    additionalButton: { title: "Инфо", link: "" },
+  },
+  {
+    cardTitle: "Аудиовызов",
+    cardSubtitle: "Мини игра",
+    cardText:
+      "Очень краткое описание игры. А возможно и статистика игрока. Типа, сыграно - столько, уровень - такой, изучено слов — столько.",
+    mainButton: { title: "Играть", link: "/games/audio-call" },
+    additionalButton: { title: "Инфо", link: "" },
+  },
+  {
+    cardTitle: "Спринт",
+    cardSubtitle: "Мини игра",
+    cardText:
+      "Очень краткое описание игры. А возможно и статистика игрока. Типа, сыграно - столько, уровень - такой, изучено слов — столько.",
+    mainButton: { title: "Играть", link: "/games/sprint" },
+    additionalButton: { title: "Инфо", link: "" },
+  },
+  {
+    cardTitle: "Своя игра",
+    cardSubtitle: "Мини игра",
+    cardText:
+      "Очень краткое описание игры. А возможно и статистика игрока. Типа, сыграно - столько, уровень - такой, изучено слов — столько.",
+    mainButton: { title: "Играть", link: "/games/our-game" },
+    additionalButton: { title: "Инфо", link: "" },
+  },
+];
+
+export default {
+  data: () => ({
+    cards,
+  }),
+};
 </script>
 
 <style scoped>
