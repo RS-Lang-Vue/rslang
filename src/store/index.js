9789436cce8334a58ea4bd1ce8b9ea13438d99ba
Maxim <foba1995@gmail.com>
2020-07-04 22:30:24 +0400

    feat:approve changes

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,8 @@ import user from "./modules/user";
 import userSettings from "./modules/user-settings";
 import common from "./modules/common";
 import game from "./modules/game";
-<<<<<<< HEAD
-=======
 import apiService from "./modules/api-service/api-service";
 import wordHelper from "./modules/word-helper";
->>>>>>> develop
 
 import englishPuzzle from "./modules/english-puzzle/english-puzzle";
 
@@ -22,10 +19,7 @@ export default new Vuex.Store({
     game,
     common,
     englishPuzzle,
-<<<<<<< HEAD
-=======
     apiService,
     wordHelper,
->>>>>>> develop
   },
 });
