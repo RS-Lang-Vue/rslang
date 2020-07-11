e12a4f4e1a258618314bc8f79e2f2a3d21795c29
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-07-11 12:37:06 +0400

    feat: add loader

--- src/views/vocabulary/Index.vue
+++ src/views/vocabulary/Index.vue
@@ -1,55 +1,66 @@
 <template>
   <div>
-    <v-tabs
-      v-model="tab"
-      background-color="deep-purple accent-4"
-      class="elevation-2"
-      dark
-      :centered="true"
-      :grow="true"
-      :vertical="false"
-      :right="false"
-      :prev-icon="undefined"
-      :next-icon="undefined"
-      :icons-and-text="icons"
-    >
-      <v-tabs-slider></v-tabs-slider>
+    <div v-if="loading">
+      <v-data-table
+        item-key="name"
+        class="elevation-1"
+        loading
+        loading-text="Loading... Please wait"
+      ></v-data-table>
+    </div>
+    <div v-else>
+      <v-tabs
+        v-model="tab"
+        background-color="deep-purple accent-4"
+        class="elevation-2"
+        dark
+        :centered="true"
+        :grow="true"
+        :vertical="false"
+        :right="false"
+        :prev-icon="undefined"
+        :next-icon="undefined"
+        :icons-and-text="icons"
+      >
+        <v-tabs-slider></v-tabs-slider>
 
-      <v-tab v-for="i in tabs" :key="i" :href="`#tab-${i}`">
-        {{ tabNames[i] }}
-      </v-tab>
+        <v-tab v-for="i in tabs" :key="i" :href="`#tab-${i}`">
+          {{ tabNames[i] }}
+        </v-tab>
 
-      <v-tab-item v-for="i in tabs" :key="i" :value="'tab-' + i">
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-center">Слово</th>
-                <th class="text-center">Транскрипция</th>
-                <th class="text-center">Перевод</th>
-                <th class="text-center">Повторялось</th>
-                <th class="text-center">Дата последнего повторения</th>
-                <th class="text-center">Дата планируемого повторения</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in words" :key="item.name">
-                <td>{{ item.word }}</td>
-                <td>{{ item.transcription }}</td>
-                <td>{{ item.wordTranslate }}</td>
-                <td>{{ item.userWord.optional.repeatCount }}</td>
-                <td>
-                  {{ new Date(item.userWord.optional.lastDate).toLocaleString("ru-Ru") }}
-                </td>
-                <td>
-                  {{ new Date(item.userWord.optional.repeatDate).toLocaleString("ru-Ru") }}
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-tab-item>
-    </v-tabs>
+        <v-tab-item v-for="i in tabs" :key="i" :value="'tab-' + i">
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-center">Слово</th>
+                  <th class="text-center">Транскрипция</th>
+                  <th class="text-center">Перевод</th>
+                  <th class="text-center">Повторялось</th>
+                  <th class="text-center">Дата последнего повторения</th>
+                  <th class="text-center">Дата планируемого повторения</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in words" :key="item.name">
+                  <td>{{ item.word }}</td>
+                  <td>{{ item.transcription }}</td>
+                  <td>{{ item.wordTranslate }}</td>
+                  <td>{{ item.userWord.optional.repeatCount }}</td>
+                  <td>
+                    {{ new Date(item.userWord.optional.lastDate).toLocaleString("ru-Ru") }}
+                  </td>
+                  <td>
+                    {{ new Date(item.userWord.optional.repeatDate).toLocaleString("ru-Ru") }}
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-tab-item>
+      </v-tabs>
+    </div>
+
     Vocabulary page
     {{ cuirrentDifficulty }}
     {{ cuirrentDifficultyCopy }}
@@ -61,11 +72,12 @@
 export default {
   data: () => ({
     tabNames: ["Изучаемые слова", "Сложные слова", "Удалённые слова"],
-    tab: null,
+    tab: "tab-0",
     icons: false,
     tabs: [0, 1, 2],
     cuirrentDifficulty: 0,
     words: [],
+    loading: true,
   }),
   async mounted() {
     await this.getWords(this.tab.split("-")[1]);
@@ -78,6 +90,7 @@ export default {
   },
   watch: {
     tab(val) {
+      this.loading = true;
       this.getWords(val.split("-")[1]);
     },
   },
@@ -94,6 +107,7 @@ export default {
         this.showAlert("error", "Ошибка!", res.error);
       }
       this.words = res.result;
+      this.loading = false;
     },
     showAlert(type, title, text) {
       this.$notify({
