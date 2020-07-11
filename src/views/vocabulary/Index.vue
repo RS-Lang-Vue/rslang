3ff97debf45eb3266178e96ebcdcf05415889f4b
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-07-11 11:58:47 +0400

    feat: add table words base

--- src/views/vocabulary/Index.vue
+++ src/views/vocabulary/Index.vue
@@ -21,9 +21,34 @@
       </v-tab>
 
       <v-tab-item v-for="i in tabs" :key="i" :value="'tab-' + i">
-        <v-card flat tile>
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-center">Слово</th>
+                <th class="text-center">Транскрипция</th>
+                <th class="text-center">Перевод</th>
+                <th class="text-center">Повторялось</th>
+                <th class="text-center">Дата последнего повторения</th>
+                <th class="text-center">Дата планируемого повторения</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in words" :key="item.name">
+                <td>{{ item.word }}</td>
+                <td>{{ item.transcription }}</td>
+                <td>{{ item.wordTranslate }}</td>
+                <td>{{ item.userWord.optional.repeatCount }}</td>
+                <td>
+                  {{ new Date(item.userWord.optional.lastDate).toLocaleString("ru-Ru") }}
+                </td>
+                <td>
+                  {{ new Date(item.userWord.optional.repeatDate).toLocaleString("ru-Ru") }}
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
       </v-tab-item>
     </v-tabs>
     Vocabulary page
@@ -38,15 +63,30 @@ export default {
   data: () => ({
     tabNames: ["Изучаемые слова", "Сложные слова", "Удалённые слова"],
     tab: null,
-    text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     icons: false,
     tabs: [0, 1, 2],
     cuirrentDifficulty: 0,
+    words: [],
   }),
+  async mounted() {
+    await this.getWords();
+  },
   computed: {
     cuirrentDifficultyCopy() {
       return this.cuirrentDifficulty + 1;
+    },
+  },
+  methods: {
+    async getWords() {
+      const res = await this.$store.dispatch("getUserAggregateWords", {
+        page: 0,
+        wordsPerPage: 20,
+        difficulty: 0,
+      });
+      if (!res.success) {
+        alert(res.error);
+      }
+      this.words = res.result;
     },
   },
 };
