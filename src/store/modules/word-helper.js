72ae90955b94018f0a3100cfa370cf9a09a9ceb9
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-07-14 22:23:16 +0400

    fix: word-helper

--- src/store/modules/word-helper.js
+++ src/store/modules/word-helper.js
@@ -12,28 +12,7 @@ export default {
       }
       let { userWord } = res.result;
       const isNewWord = !userWord;
-<<<<<<< HEAD
       userWord = updateUserWord(userWord, changes);
-=======
-      if (isNewWord) {
-        userWord = {
-          difficulty: "0",
-          optional: {
-            correctAnswer: 0,
-            repeatCount: 0,
-            lastDate: 0,
-            repeatDate: 0,
-            description: "0",
-          },
-        };
-      }
-      userWord.optional.repeatCount += 1;
-      userWord.optional.lastDate = Date.now();
-      userWord.optional.repeatDate = Date.now() + (isCorrectAnswer ? 50000 : 10000);
-      if (userWord.difficulty === "hard") userWord.difficulty = "0";
-      if (userWord.optional.correctAnswer === undefined) userWord.optional.correctAnswer = 0;
-      if (isCorrectAnswer) userWord.optional.correctAnswer += 1;
->>>>>>> 62b2f8deecc22850bf5e50697d40c0028c45c641
       res = await this.dispatch("setUserWords", { isNewWord, userWord, wordId });
       if (!res.success) {
         return res;
